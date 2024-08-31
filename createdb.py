d1a95bfbbaea09023f6aac9be76dbfc0661ff567
jhbhan <43838586+jhbhan@users.noreply.github.com>
2024-08-31 08:49:06 -0700

    Initial commit

---
+++ createdb.py
@@ -0,0 +1,49 @@
+import json
+import os
+import sqlite3
+import time
+
+NT_FILE_PATH = './EN/NT'
+OT_FILE_PATH = './EN/OT'
+
+conn = sqlite3.connect('bible.db')
+cursor = conn.cursor()
+
+# Function to insert JSON data into the SQLite database
+def insert_data(file_path):
+    with open(file_path) as f:
+        data = json.load(f)
+        for chapter in data['chapters']:
+            for verse in chapter['verses']:
+                cursor.execute('''INSERT INTO verses (book, chapter, verse, content)
+                                  VALUES (?, ?, ?, ?)''', (data['bookName'], int(chapter['chapter']), int(verse['verse']), verse['content']))
+
+
+def create_bible_db():
+    # Insert data from JSON files to the SQLite database
+    # Insert New Testament
+    for file_name in os.listdir(NT_FILE_PATH):
+        if file_name.endswith('.json'):
+            insert_data(os.path.join(NT_FILE_PATH, file_name))
+
+    # Insert Old Testament
+    for file_name in os.listdir(OT_FILE_PATH):
+        if file_name.endswith('.json'):
+            insert_data(os.path.join(OT_FILE_PATH, file_name))
+
+
+# Create a table to store Bible verses
+cursor.execute('''CREATE TABLE IF NOT EXISTS verses
+             (id INTEGER PRIMARY KEY AUTOINCREMENT,
+              book TEXT,
+              bookId INTEGER,
+              chapter INTEGER,
+              verse INTEGER,
+              content TEXT)''')
+
+
+create_bible_db()
+
+# Commit changes and close the connection
+conn.commit()
+conn.close()
---
+++ main.py
@@ -0,0 +1,60 @@
+import datetime
+
+import sqlite3
+from flask import Flask, request, jsonify, render_template, g
+
+app = Flask(__name__)
+
+DATABASE = 'bible.db'
+
+def get_db():
+    # Open a new database connection if there is none yet for the current application context
+    if 'db' not in g:
+        g.db = sqlite3.connect(DATABASE)
+    return g.db
+
+@app.teardown_appcontext
+def close_db(error):
+    # Close the database again at the end of the request
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
+
+@app.route("/")
+def root():
+    # For the sake of example, use static information to inflate the template.
+    # This will be replaced with real information in later steps.
+    dummy_times = [
+        datetime.datetime(2018, 1, 1, 10, 0, 0),
+        datetime.datetime(2018, 1, 2, 10, 30, 0),
+        datetime.datetime(2018, 1, 3, 11, 0, 0),
+    ]
+
+    return render_template("index.html", times=dummy_times)
+
+@app.route("/search", methods=['GET'])
+def search_keywords():
+    keyword = request.args.get('keyword', '')
+    if not keyword:
+        return jsonify("No keyword provided")
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM verses WHERE LOWER(content) LIKE ?', ('%' + keyword.lower() + '%',))
+    results = cursor.fetchall()
+    # Convert results into a list of dictionaries
+    results = [{'id': row[0], 'book': row[1], 'chapter': row[2], 'verse': row[3], 'content': row[4]} for row in results]
+    return jsonify(results)
+
+@app.route('/api', methods=['GET'])
+def api():
+    return jsonify("API is working")
+
+if __name__ == "__main__":
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    # Flask's development server will automatically serve static files in
+    # the "static" directory. See:
+    # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
+    # App Engine itself will serve those files as configured in app.yaml.
+    app.run(host="127.0.0.1", port=8080, debug=True)
